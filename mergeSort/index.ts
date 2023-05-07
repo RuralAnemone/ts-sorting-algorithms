5b09cef56052fd02f6fabe6f65d21bd302e000d6
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2023-05-06 21:00:36 -0500

    um

--- mergeSort/index.ts
+++ mergeSort/index.ts
@@ -1,4 +1,4 @@
-export default function mergeSort(inputArray: number[]): number[] {
+function mergeSort(inputArray: number[]): number[] {
 	if (inputArray.length === 1) return inputArray;
 	
 	let leftHalf: number[] = inputArray.filter((_e, i) => i < inputArray.length / 2);
@@ -20,3 +20,10 @@ export default function mergeSort(inputArray: number[]): number[] {
 
 	return sortedArray;
 }
+
+const arr0: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const arr1: number[] = [1,5,0,7,9,8,6,2,3,4];
+
+debugger;
+
+mergeSort(arr0);
