80526173e3f713d2798acd6e1f1122a7e78c4f0d
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2023-05-07 20:31:42 -0500

    fix edge cases, add jsdoc I think that's too much jsdoc

--- mergeSort/index.ts
+++ mergeSort/index.ts
@@ -1,5 +1,30 @@
+/**
+ * ## Merge Sort
+ * features:
+ * - recursive
+ * - $\Theta(n \cdot log_{2}(n))$
+ * 
+ * ---
+ * 
+ * ### How it works:
+ * 
+ * 1. Sorts the left half of the array using Merge Sort
+ * 2. Sorts the right half of the array using Merge Sort
+ * 3. Merges the two halves together
+ *    - Goes through both sorted halves, and puts them into a new array in ascending order
+ * 
+ * (edge cases)
+ * 
+ * - if `inputArray.length` <= 1, immediately `return inputArray`
+ *   - (i.e. can't split array further; therefore it's already sorted);
+ * 
+ * ---
+ * 
+ * @param inputArray unsorted array to be sorted
+ * @returns a sorted array
+ */
 function mergeSort(inputArray: number[]): number[] {
-	if (inputArray.length === 1) return inputArray;
+	if (inputArray.length <= 1) return inputArray;
 	
 	let leftHalf: number[] = inputArray.filter((_e, i) => i < inputArray.length / 2);
 	let rightHalf: number[] = inputArray.filter((_e, i) => i >= inputArray.length / 2);
